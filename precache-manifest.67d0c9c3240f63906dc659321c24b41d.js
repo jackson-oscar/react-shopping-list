711e7debbcee1de63da2ca9b6d43fef0572c609a
jackson.oscar <ojackson7@users.noreply.github.com>
2020-01-12 18:19:14 -0800

    Updates

---
+++ precache-manifest.67d0c9c3240f63906dc659321c24b41d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f376c3191b8d1e4843e02f1b4277b8d6",
+    "url": "/react-shopping-list/index.html"
+  },
+  {
+    "revision": "0b65ea895e0342e0c67f",
+    "url": "/react-shopping-list/static/css/main.603e02a3.chunk.css"
+  },
+  {
+    "revision": "af338ea153ee5047e9dc",
+    "url": "/react-shopping-list/static/js/2.37950f49.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react-shopping-list/static/js/2.37950f49.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b65ea895e0342e0c67f",
+    "url": "/react-shopping-list/static/js/main.e9f3b977.chunk.js"
+  },
+  {
+    "revision": "2e4d1b1b641262e24134",
+    "url": "/react-shopping-list/static/js/runtime-main.6e8304c2.js"
+  }
+]);
